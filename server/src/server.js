0e89dfea9ccc4bf64aea99a8ffadad3ed52b83dc
thiernomoudou <souleymanemoudou@gmail.com>
2017-11-27 17:15:50 +0000

    Add API documentation using swagger

--- server/src/server.js
+++ server/src/server.js
@@ -20,8 +20,38 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(port, () =>{
-  console.log(`Listening on port ${port}`);
-});
+
+//Swagger Docs
+import swaggerTools from 'swagger-tools';
+// swaggerRouter configuration
+let options = {
+	swaggerUi: '/swagger.json',
+	// controllers: './controllers'
+}
+
+// The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
+import swaggerDoc from '../../swagger.json';
+
+// Initialize the Swagger middleware
+swaggerTools.initializeMiddleware(swaggerDoc, (middleware)=>{
+	// Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
+	app.use(middleware.swaggerMetadata());
+	 // Validate Swagger requests
+	 app.use(middleware.swaggerValidator());
+	 // Route validated requests to appropriate controller
+    app.use(middleware.swaggerRouter(options));
+    // Serve the Swagger documents and Swagger UI
+    app.use(middleware.swaggerUi());
+
+    // start the server
+    app.listen(port, () =>{
+	  console.log(`Listening on port ${port}`);
+	 });
+
+    });
+
+
+
+
 
 export {app, recipeController};
